23e7a6043b9e846df05c496404cc30cd23c6305f
Neerajistheboss <neerajistheboss@gmail.com>
2022-11-01 22:55:54 +0530

    route updated for github pages

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<DashboardPage />}>
+        <Route path='/geniobits' element={<DashboardPage />}>
         </Route>
       </Routes>
     </Router>
